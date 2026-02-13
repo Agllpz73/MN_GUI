24b99de9b291568e25b76f838a641036275d4c24
Agllpz73 <lopezdiaz1bm@gmail.com>
2026-02-12 20:49:55 -0600

    correcion en algunas rutas

--- nummethods/routes.py
+++ nummethods/routes.py
@@ -9,40 +9,40 @@ def home():
 
 @main_bp.get("/ec-no-lineales")
 def non_linear_eq():
-    return render_template("sections/placeholder.html", title="Ecuaciones no lineales")
+    return render_template("calculator.html", title="Ecuaciones no lineales")
 
 @main_bp.get("/sist-ec-no-lineales")
 def non_linear_systems():
-    return render_template("sections/placeholder.html", title="Sistemas de ecuaciones no lineales")
+    return render_template("calculator.html", title="Sistemas de ecuaciones no lineales")
 
 @main_bp.get("/sist-ec-lineales")
 def linear_systems():
-    return render_template("sections/placeholder.html", title="Sistemas de ecuaciones lineales")
+    return render_template("calculator.html", title="Sistemas de ecuaciones lineales")
 
 @main_bp.get("/factorizacion")
 def factorization():
-    return render_template("sections/placeholder.html", title="Factorización")
+    return render_template("calculator.html", title="Factorización")
 
 @main_bp.get("/iterativos")
 def iterative():
-    return render_template("sections/placeholder.html", title="Métodos iterativos")
+    return render_template("calculator.html", title="Métodos iterativos")
 
 @main_bp.get("/interpolacion")
 def interpolation():
-    return render_template("sections/placeholder.html", title="Interpolación / Aproximación")
+    return render_template("calculator.html", title="Interpolación / Aproximación")
 
 @main_bp.get("/integracion")
 def integration():
-    return render_template("sections/placeholder.html", title="Integración numérica")
+    return render_template("calculator.html", title="Integración numérica")
 
 @main_bp.get("/edo")
 def ode():
-    return render_template("sections/placeholder.html", title="EDO")
+    return render_template("calculator.html", title="EDO")
 
 @main_bp.get("/<category>")
 def category(category):
     # página por categoría (cards, descripción, etc.)
-    return render_template("sections/category.html", category=category)
+    return render_template("calculator.html", category=category)
 """
 @main_bp.get("/<category>/<method>")
 def method_view(category, method):
@@ -87,6 +87,7 @@ def method_view(category, method):
         "euler": "methods/euler.html",
         "runge-kutta": "methods/runge_kutta.html",
         "euler-mejorado": "methods/euler_mejorado.html"
+
     }
 
     if method not in methods:
